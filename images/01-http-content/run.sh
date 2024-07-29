7e285e2ceda1f6bb08789672d68d80bbd88b3aec
nagesha technology <asbconstruction19@gmail.com>
2024-07-29 14:38:53 +0530

    kube init-container

--- images/01-http-content/run.sh
+++ images/01-http-content/run.sh
@@ -1,5 +1,6 @@
 mkdir -p /usr/share/nginx/html
-while true ; do
-  echo "<h1> Hellow - $(date)</h1>" >/usr/share/nginx/html/index.html
-  sleep 1
-done
+#while true ; do
+#  echo "<h1> Hellow - $(date)</h1>" >/usr/share/nginx/html/index.html
+#  sleep
+#done
+echo Hello World >/usr/share/nginx/html/index.html
